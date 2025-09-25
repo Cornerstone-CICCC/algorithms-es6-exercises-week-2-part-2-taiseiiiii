73d9a2f2e9dce7714a04b086f94abc8b856fb84c
taiseiiiii <isemiya.0509@gmail.com>
2025-09-24 20:30:14 -0700

    complete ex05

--- ex05.js
+++ ex05.js
@@ -1,3 +1,9 @@
 // Some Exercise
 // Instructions: Create an array of at least five numbers. Write code to check if some of the numbers are even.
+const target = [1, 2, 3, 4, 5];
 
+console.log(
+  target.some((e) => e % 2 === 0)
+    ? "Some of numbers are even"
+    : "No even numbers found"
+);
